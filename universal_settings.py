5eca15ca70e0f0638bc32567c515ba0ec3d49ac0
Oliver Newton <oliver.newton.92+github@gmail.com>
2023-04-01 20:14:43 +0200

    Abstracting settings to a universal_settings file. Generating commands dynamically

---
+++ universal_settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+# Cosmology
+h = 0.73  # Hubble factor for the WMAP-1 cosmology used by Aquarius.
+
+# File locations
+all_sat_data = os.path.join('Input_Data', 'All_satellites.csv')
+input_data = 'Input_Data'
+output_data = 'Output_Data'
+
+# Analysis settings
+r_out = 300.
+r_out_text = '300'
+
+# Mass of the Aquarius haloes in units of 'Msun/h'.
+aq_mass = {
+    'Aq-A': 1.839e12 * h,
+    'Aq-B': 0.819e12 * h,
+    'Aq-C': 1.774e12 * h,
+    'Aq-D': 1.774e12 * h,
+    'Aq-E': 1.185e12 * h,
+}
+
+# A few values for the MW halo mass
+MW_mass = {
+    'small': [0.5e12 * h, '0.5e12'],
+    'medium': [1.0e12 * h, '1.0e12'],
+    'large': [2.0e12 * h, '2.0e12'],
+}
