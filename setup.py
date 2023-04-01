0caee6445445d763b429fa59593045e41978dd91
Oliver Newton <oliver.newton.92+github@gmail.com>
2023-04-01 19:06:01 +0200

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
-raise NotImplementedError("Setup not implemented yet.")
+
+from setuptools import setup
+
+cmdclass = {}
+ext_modules = []
+
+name = 'mw_sats_lf'
+version = '1.0'
+release = '1.0.0'
+
+setup(
+    name=name,
+    version=version,
+    description='Codes to generate MW satellite galaxy luminosity functions',
+    author='Oliver Newton',
+    author_email='onewton@cft.edu.pl',
+    packages=['mw_sats_lf'],
+    cmdclass=cmdclass,
+    ext_modules=ext_modules,
+    command_options={},
+)
